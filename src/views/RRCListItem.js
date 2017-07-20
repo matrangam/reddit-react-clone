5420ceb89060c0309b228e612657fcd6ee539661
Michael Matranga <mamatranga@gmail.com>
2017-07-20 15:27:47 -0400

    Added title.

--- src/views/RRCListItem.js
+++ src/views/RRCListItem.js
@@ -4,6 +4,11 @@ function RRCListItem(props) {
     if (props.size === 0) {
         return <li></li>
     } 
-    return <li key={props.name}> <img src={props.url} alt={props.title}/> </li>
+    return ( 
+        <li key={props.name}> 
+            <p>{props.title}</p>
+            <img src={props.url} alt={props.title}/> 
+        </li>
+    );
 }
 export default RRCListItem
