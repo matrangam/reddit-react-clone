d1034e35bcc7838bc6d2aa1c8232bce7a7561ef7
Michael Matranga <mamatranga@gmail.com>
2017-07-20 18:26:46 -0400

    Installed roboto font.

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -2,6 +2,7 @@ import AppView from '../views/AppView';
 import {Container} from 'flux/utils';
 import RRCStore from '../data/RRCStore';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import 'roboto-npm-webfont';
 
 function getStores() {
   return [
